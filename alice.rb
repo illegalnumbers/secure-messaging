aa8cef333596f4e0bff8a742196bc24c5ff4ddb5
Ben Nelson <nelson.ben.c@gmail.com>
2013-07-08 19:51:24 -0600

    adding in basic certificate and testing symmetric key stuff.

--- alice.rb
+++ alice.rb
@@ -1,11 +1,36 @@
+#used for connecting to hosts
 require 'socket'
+#crypto library
+require 'openssl'
+#used for generating random numbers
+require 'securerandom'
 
-hostname = 'lab1-15.eng.utah.edu'
+host = 'lab1-15.eng.utah.edu'
 port = 9090
 
 s = TCPSocket.open(host, port)
 
+#get public key certificate from bob
 while line = s.gets
 	puts line.chop
 end
+
+#verify the certificate with the given public key
+raise 'certificate can not be verified' unless cert2.verify key
+
+
+# For testing purposes only!
+ message = 'MyTestString'
+ #i think key should be alice's private key
+ key = 'PasswordPasswordPassword'
+ iv = '12345678'
+
+# # Encrypt plaintext using Triple DES
+ cipher = OpenSSL::Cipher::Cipher.new("des3")
+ cipher.encrypt # Call this before setting key or iv
+ cipher.key = key
+ cipher.iv = iv
+ ciphertext = cipher.update(message)
+ ciphertext << cipher.final
+
 s.close
--- bob.rb
+++ bob.rb
@@ -1,13 +1,43 @@
 require 'socket'
+require 'openssl'
 
 hostname = 'lab1-16.eng.utah.edu'
 port = 9090
 
 s = TCPServer.open(port)
 
+#make new private / public rsa key-pair
+key = OpenSSL::PKey::RSA.new 2048
+#write key-pair to disk
+open 'private_key.pem', 'w' do |io| io.write key.to_pem end
+open 'public_key.pem', 'w' do |io| io.write key.public_key.to_pem end
+
+#create signed certificate using rsa and sha1 signature
+name = OpenSSL::X509::Name.parse 'CN=nobody/DC=example'
+
+cert = OpenSSL::X509::Certificate.new
+cert.version = 2
+cert.serial = 0
+cert.not_before = Time.now
+cert.not_after = Time.now + 3600
+
+cert.public_key = key.public_key
+cert.subject = name
+cert.issuer = name
+cert.sign key, OpenSSL::Digest::SHA1.new
+
+#write the certificate to file
+open 'certificate.pem', 'w' do |io| io.write cert.to_pem end
+
 loop {
-	client = server.accept
+	client = s.accept
 	client.puts(Time.now.ctime)
-	clients.puts "Now you die, connection."
+	client.puts "Now you die, connection."
 	client.close
 }
+
+#assuming you now have the symmetric key from alice in cipher
+#and the encodedCipherText
+cipher.decrypt
+plaintext = cipher.update(encodedCipherText)
+plaintext << cipher.final
