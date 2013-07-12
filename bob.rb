843f2835bccdd6a8d3ac38f44a8bb15c0650c452
Ben Nelson <nelson.ben.c@gmail.com>
2013-07-11 22:23:16 -0600

    adding in decryption

--- bob.rb
+++ bob.rb
@@ -7,30 +7,48 @@ port = 9090
 s = TCPServer.open(port)
 
 #make new private / public rsa key-pair
-key = OpenSSL::PKey::RSA.new 2048
+rsakey = OpenSSL::PKey::RSA.new 2048
 
 #write key-pair to disk
-open 'bob_private_key.pem', 'w' do |io| io.write key.to_pem end
-open 'bob_public_key.pem', 'w' do |io| io.write key.public_key.to_pem end
+open 'bob_private_key.pem', 'w' do |io| io.write rsakey.to_pem end
+open 'bob_public_key.pem', 'w' do |io| io.write rsakey.public_key.to_pem end
 
 #hash the key using sha1
 sha1 = OpenSSL::Digest::SHA1.new
-digest = sha1.digest(key.public_key.to_pem)
+digest = sha1.digest(rsakey.public_key.to_pem)
 
 pubkey = JSON.generate({
-	key: key.public_key.to_pem
+	key: rsakey.public_key.to_pem
 	digest: digest
 	})
 
 loop {
 	client = s.accept
+	#get public key from alice
 	incoming = client.gets()
 	alice = JSON.parse(incoming)
-
 	alice_key = OpenSSL::PKey::RSA.new alice['key']
 
+	#send public key to alice
 	puts pubkey
 	client.puts pubkey	
+
+	#get encrypted package from alice
+	json_full_package = client.gets()
+	full_package = JSON.parse(json_full_package)
+
+	#decrypt and print package	
+	cipher = OpenSSL::Cipher.new("DES3")
+	cipher.decrypt
+	key = rsakey.private_decrypt(full_package['key'])
+	iv = rsakey.private_decrypt(full_package['iv'])
+	json_package = cipher.update(full_package['package'])
+
+	package = JSON.parse(json_package)
+	decrypted_digest = alice_key.public_decrypt(package['signed_digest'])
+	sha1 = OpenSSL::Digest::SHA1.new
+	digest = sha1.digest(package['data'])
+	throw 'failed digest' unless digest == decrypted_digest
 
 	client.close
 }
