797b924015e4bfd78ceb73672ee01b4b3b8ccf2d
Ben Nelson <nelson.ben.c@gmail.com>
2013-07-11 22:10:48 -0600

    finally on the right track! almost done. implemented everything except decrypting

--- alice.rb
+++ alice.rb
@@ -1,48 +1,71 @@
-#used for connecting to hosts
 require 'socket'
-#crypto library
 require 'openssl'
-#used for generating random numbers
-require 'securerandom'
+require 'json'
+
 host = 'lab1-15.eng.utah.edu'
 port = 9090
 
 s = TCPSocket.open(host, port)
 
 pubkey_q = false
-cert_q = false
 
 keyF = File.new("public_key.pem", 'w')
-certF = File.new("certificate.pem", 'w')
+
+#generate alice's key pair
+key = OpenSSL::PKey::RSA.new 2048
+
+to_bob_public = JSON.generate({
+	key: key.public_key.to_pem
+	})
+s.send(to_bob_public)
 
 #get public key certificate from bob
 while line = s.gets
 	puts line.chop
-
-	if(/BEGIN PUBLIC KEY/.match(line))
-		pubkey_q = true
-	end	
-	if(/END PUBLIC KEY/.match(line))
-		pubkey_q = false
-	end
-	if(/BEGIN CERTIFICATE/.match(line))
-		cert_q = true
-	end
-	if(/END CERTIFICATE/.match(line))
-		cert_q = false
-	end
-
-	if(pubkey_q)
-		keyF.write(line)
-	elsif cert_q
-		certF.write(line)
-	end
+	bob = JSON.parse(line)
 end
+bob_key = OpenSSL::PKey::RSA.new bob['key']
+bob_digest = bob['digest']
 
-key = OpenSSL::PKey::RSA.new File.read 'public_key.pem'
-raw = File.new('certificate.pem', 'r')
-certificate = OpenSSL::X509::Certificate.new raw
-raise 'certificate can not be verified' unless cert2.verify key
-puts 'YAY the certificate was valid'
+#verify public key
+sha1 = OpenSSL::Digest::SHA1.new
+t_digest = sha1.digest(bob['key'])
+throw "not verified" unless t_digest == bob_digest
+
+data = File.read('document') #data is original message
+
+#hash the document using sha1
+sha1 = OpenSSL::Digest::SHA1.new
+digest = sha1.digest(data)
+
+#sign with private key
+signed_digest = key.private_encrypt(digest)
+
+#package this in json
+package = JSON.generate({
+		signed_digest: signed_digest,
+		data: data		
+	})
+
+#make cipher for encryption
+cipher = OpenSSL::Cipher.new("DES3")
+cipher.encrypt
+key = cipher.random_key
+iv = cipher.random_iv
+#encrypt data
+encrypted = cipher.update(package) 
+
+#encrypt key and iv using bob's public key
+encrypted_cipher_key = bob_key.public_encrypt(key)
+encrypted_cipher_iv = bob_key.public_encrypt(iv)
+
+full_package = JSON.generate({
+		key: encrypted_cipher_key,
+		iv: encrypted_cipher_iv,
+		package: encrypted		
+	})
+
+#send full_package to bob
+s.send(full_package)
 
 s.close
--- bob.rb
+++ bob.rb
@@ -1,43 +1,36 @@
 require 'socket'
 require 'openssl'
-hostname = 'lab1-16.eng.utah.edu'
+require 'json'
+
 port = 9090
 
 s = TCPServer.open(port)
 
 #make new private / public rsa key-pair
 key = OpenSSL::PKey::RSA.new 2048
+
 #write key-pair to disk
-open 'private_key.pem', 'w' do |io| io.write key.to_pem end
-open 'public_key.pem', 'w' do |io| io.write key.public_key.to_pem end
+open 'bob_private_key.pem', 'w' do |io| io.write key.to_pem end
+open 'bob_public_key.pem', 'w' do |io| io.write key.public_key.to_pem end
 
-#create signed certificate using rsa and sha1 signature
-name = OpenSSL::X509::Name.parse 'CN=nobody/DC=example'
+#hash the key using sha1
+sha1 = OpenSSL::Digest::SHA1.new
+digest = sha1.digest(key.public_key.to_pem)
 
-cert = OpenSSL::X509::Certificate.new
-cert.version = 2
-cert.serial = 0
-cert.not_before = Time.now
-cert.not_after = Time.now + 3600
-
-cert.public_key = key.public_key
-cert.subject = name
-cert.issuer = name
-cert.sign key, OpenSSL::Digest::SHA1.new
-
-#write the certificate to file
-open 'certificate.pem', 'w' do |io| io.write cert.to_pem end
+pubkey = JSON.generate({
+	key: key.public_key.to_pem
+	digest: digest
+	})
 
 loop {
 	client = s.accept
+	incoming = client.gets()
+	alice = JSON.parse(incoming)
 
-	pubkey = IO.read('public_key.pem')
+	alice_key = OpenSSL::PKey::RSA.new alice['key']
+
 	puts pubkey
 	client.puts pubkey	
-
-	certFile = IO.read('certificate.pem')
-	puts certFile
-	client.puts certFile
 
 	client.close
 }
