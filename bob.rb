0a18ef7d9d51f4c078dd1f8dbcd3f44458918b7a
Ben Nelson <nelson.ben.c@gmail.com>
2013-07-12 20:15:46 -0600

    ?

--- bob.rb
+++ bob.rb
@@ -46,7 +46,7 @@ loop {
 	puts "decrypted key is #{key}"
 	iv = rsakey.private_decrypt(Base64.decode64(full_package['iv']))
 	puts "decrypted iv is #{iv}"
-	json_package = cipher.update(Base64.decode64(full_package['package'])) << cipher.final
+	json_package = cipher.update(Base64.decode64(full_package['package'])) 
 	puts "decrypted package is #{json_package}"	
 
 	package = JSON.parse(json_package)
