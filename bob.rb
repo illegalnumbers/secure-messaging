848ab42f47c1b16131dd082e58870f7fbe82d117
Benjamin C Nelson <benjamin@lab1-15.eng.utah.edu>
2013-07-12 22:26:33 -0600

    fixed issue, wasn't setting cipher key/iv

--- bob.rb
+++ bob.rb
@@ -40,22 +40,25 @@ loop {
 	puts "received package from alice"
 	puts "#{key}"
 	puts "#{iv}"
-	puts "#{data}"
+	puts "#{e_data}"
 	#puts "package contents #{json_full_package}"
 	#full_package = JSON.parse(json_full_package)
 
 	#decrypt and print package	
 	cipher = OpenSSL::Cipher.new("DES3")
 	cipher.decrypt
+	cipher.key = key
+	cipher.iv = iv
 	#key and iv should rsakey.private_decrypt
-	key = (Base64.decode64(full_package['key']))
+	#key = (Base64.decode64(full_package['key']))
 	puts "decrypted key is #{key}"
-	iv = (Base64.decode64(full_package['iv']))
+	#iv = (Base64.decode64(full_package['iv']))
 	puts "decrypted iv is #{iv}"
-	json_package = cipher.update((full_package['package'])) << cipher.final
+	#json_package = cipher.update(full_package['package'])) << cipher.final
+	json_package = cipher.update(e_data)
 	puts "decrypted package is #{json_package}"	
 
-	package = JSON.parse(json_package)
+	#package = JSON.parse(json_package)
 	decrypted_digest = alice_key.public_decrypt(Base64.decode64(package['signed_digest']))
 	sha1 = OpenSSL::Digest::SHA1.new
 	digest = sha1.hexdigest(package['data'])
